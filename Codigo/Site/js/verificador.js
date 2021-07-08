5e473f94d5c1706abc1a2730154b0b0312108360
Bernardo <bernardoaquinoc@gmail.com>
2021-07-08 18:58:01 -0300

    Update

--- Codigo/Site/js/verificador.js
+++ Codigo/Site/js/verificador.js
@@ -1,46 +1,48 @@
 $("#btnVerificar").click(() => {
-  axios.get(`https://api-tiaw.bernardoaquino1.repl.co/sitesVerificador`).then((response) => {
-    const BD = response.data;
-    console.log(BD);
-    $(".container .preloader").delay(4000).removeClass("complete");
-    setTimeout(() => {
-      let param = $("#inputVerificar").val();
+  axios
+    .get(`https://api-tiaw.bernardoaquino1.repl.co/sitesVerificador`)
+    .then((response) => {
+      const BD = response.data;
 
-      if (param == "") {
-        $(".container .preloader").delay(4000).addClass("complete");
-        Swal.fire({
-          icon: "error",
-          title: "Opa...",
-          text: "Por favor preencha o campo!",
-        });
-      } else {
-        let resultado = "";
-        BD.map((item) => {
-          let link = param.split("/");
+      $(".container .preloader").delay(4000).removeClass("complete");
+      setTimeout(() => {
+        let param = $("#inputVerificar").val();
 
-          if (link.filter((value) => value === item.link).length > 0) {
-            resultado = item.link;
-          }
-        });
-
-        $(".container .preloader").delay(4000).addClass("complete");
-        if (resultado !== "") {
+        if (param == "") {
+          $(".container .preloader").delay(4000).addClass("complete");
           Swal.fire({
-            icon: "success",
-            title: "Confiável!",
-            text: "A notícia verificada é confiável!",
+            icon: "error",
+            title: "Opa...",
+            text: "Por favor preencha o campo!",
           });
         } else {
-          Swal.fire({
-            icon: "warning",
-            title: "Aviso",
-            text: "A noticia verificada é possivelmente uma Fake News!",
-            confirmButtonText: "Entendi",
-            footer:
-              '<a href="verificador.html#comunidade">Acha que isso é um erro? Preencha esse formulário</a>', //mandar para a section que o luiz criou
+          let resultado = "";
+          BD.map((item) => {
+            let link = param.split("/");
+
+            if (link.filter((value) => value === item.link).length > 0) {
+              resultado = item.link;
+            }
           });
+
+          $(".container .preloader").delay(4000).addClass("complete");
+          if (resultado !== "") {
+            Swal.fire({
+              icon: "success",
+              title: "Confiável!",
+              text: "A notícia verificada é confiável!",
+            });
+          } else {
+            Swal.fire({
+              icon: "warning",
+              title: "Aviso",
+              text: "A noticia verificada é possivelmente uma Fake News!",
+              confirmButtonText: "Entendi",
+              footer:
+                '<a href="verificador.html#comunidade">Acha que isso é um erro? Preencha esse formulário</a>', //mandar para a section que o luiz criou
+            });
+          }
         }
-      }
-    }, 2000);
-  });
+      }, 2000);
+    });
 });
